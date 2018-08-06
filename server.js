f450fb8de5282bade74482d1bb99a8cdccdd19b9
Sunny Zuo <32416882+sunny-zuo@users.noreply.github.com>
2018-08-06 16:25:59 -0600

    change to websockets from socketio

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 'use strict';
 
 const express = require('express');
-const socketIO = require('socket.io');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
+const wss = new SocketServer({ server });
 
-io.on('connection', (socket) => {
+wss.on('connection', (ws) => {
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  ws.on('close', () => console.log('Client disconnected'));
 });
