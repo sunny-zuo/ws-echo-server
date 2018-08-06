912c3c5dee63721d89a22639a208655d821f6051
Sunny Zuo <32416882+sunny-zuo@users.noreply.github.com>
2018-08-06 17:43:56 -0600

    remove webserver bit

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
