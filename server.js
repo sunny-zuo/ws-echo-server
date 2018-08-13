30a9a1a0e23587bf0e97f7e88884dd55304f434b
Sunny Zuo <me@sunnyzuo.com>
2018-08-13 16:22:33 -0600

    trying different server

--- server.js
+++ server.js
@@ -45,48 +45,20 @@ setInterval(function() {
 }, 300000); // every 5 minutes (300000)
 
 
-var xml = '<?xml version="1.0"?>\n<!DOCTYPE cross-domain-policy SYSTEM \n"http://www.adobe.com/xml/dtds/cross-domain-policy.dtd">\n<cross-domain-policy>\n';
-    xml += '<site-control permitted-cross-domain-policies="master-only"/>\n';
-    xml += '<allow-access-from domain="*"/>\n';
-    xml += '<allow-http-request-headers-from domain="*" headers="*"/>\n'
-    xml += '</cross-domain-policy>\n';
 
-
-var file = xml,
-	host = process.argv[3] || 'localhost',
-	port = process.argv[4] || 843,
-	poli;
-
-var fsps = require('net').createServer(function (stream) {
-	stream.setEncoding('utf8');
-	stream.setTimeout(3000); // 3s
-	stream.on('connect', function () {
-		console.log('Got connection from ' + stream.remoteAddress + '.');
-	});
-	stream.on('data', function (data) {
-		if (data == '<policy-file-request/>\0') {
-			console.log('Good request. Sending file to ' + stream.remoteAddress + '.')
-			stream.end(poli + '\0');
-		} else {
-			console.log('Bad request from ' + stream.remoteAddress + '.');
-			stream.end();
-		}
-	});
-	stream.on('end', function () {
-		stream.end();
-	});
-	stream.on('timeout', function () {
-		console.log('Request from ' + stream.remoteAddress + ' timed out.');
-		stream.end();
-	});
-});
-
-require('fs').readFile(file, 'utf8', function (err, poli) {
-	if (err) throw err;
-	fsps.listen(port, host);
-	process.setgid('nobody');
-	process.setuid('nobody');
-	console.log('Flash socket policy server running at ' + host + ':' + port + ' and serving ' + file);
-});
-
-    
+var netserver = net.createServer(function(socket){
+  socket.addListener("error",function(err){
+      socket.end && socket.end() || socket.destroy && socket.destroy();
+  });
+  var xml = '<?xml version="1.0"?>\n<!DOCTYPE cross-domain-policy SYSTEM \n"http://www.adobe.com/xml/dtds/cross-domain-policy.dtd">\n<cross-domain-policy>\n';
+  xml += '<site-control permitted-cross-domain-policies="master-only"/>\n';
+  xml += '<allow-access-from domain="*"/>\n';
+  xml += '<allow-http-request-headers-from domain="*" headers="*"/>\n'
+  xml += '</cross-domain-policy>\n';
+      if(socket && socket.readyState == 'open'){
+        socket.write(xml);
+        socket.end();	
+      }
+  });
+  netserver.addListener("error",function(err){}); 
+  netserver.listen(843, 'localhost');
